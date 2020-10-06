5431e4c8052095fa4ad8a1fa4fed63edebeaf277
Max <maxim.macari.dam@gmail.com>
2020-10-06 13:42:04 +0200

    Initial Commit

---
+++ OnlineShopAnimations/OnlineShopAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopAnimationsApp.swift
+//  OnlineShopAnimations
+//
+//  Created by Maxim Macari on 06/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
